8df72db51340a7d25e40e1cb4ae2fcf53bc659a3
safeplace12345 <safeplace12345@gmail.com>
2021-04-09 14:29:23 +0300

    Redirecting Requests

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   let { url, method, headers } = req;
-if(url === "/"){
+  if (url === "/") {
     res.setHeader("Content-Type", "text/html");
-    res.write(`<html><body><form action="/message"><input type="text" name="message"/><button type="submit">Send</button></form><body></html>`);
-   return res.end();
-}
+    res.write(
+      `<html><body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form><body></html>`
+    );
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "Dummy");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
   // process.exit()
   res.setHeader("Content-Type", "text/html");
   res.write(`<html><body>Hello from Node Js !!!!!<body></html>`);
