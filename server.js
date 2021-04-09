dd82014beb67889019ee36132e919e73b369654a
safeplace12345 <safeplace12345@gmail.com>
2021-04-09 14:17:41 +0300

    Routing Requests

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   let { url, method, headers } = req;
-  console.log({ url, method, headers });
+if(url === "/"){
+    res.setHeader("Content-Type", "text/html");
+    res.write(`<html><body><form action="/message"><input type="text" name="message"/><button type="submit">Send</button></form><body></html>`);
+   return res.end();
+}
   // process.exit()
   res.setHeader("Content-Type", "text/html");
   res.write(`<html><body>Hello from Node Js !!!!!<body></html>`);
