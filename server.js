11731fac969e78f2b97f2397c2aa7f7bade416c9
safeplace12345 <safeplace12345@gmail.com>
2021-04-09 14:06:45 +0300

    Retriving request headers,method,url and Handling responses

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer((req ,res) => {
-    console.log(req)
+const server = http.createServer((req, res) => {
+  let { url, method, headers } = req;
+  console.log({ url, method, headers });
+  // process.exit()
+  res.setHeader("Content-Type", "text/html");
+  res.write(`<html><body>Hello from Node Js !!!!!<body></html>`);
+  res.end();
 });
 
 server.listen(3000);
