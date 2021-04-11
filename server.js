f7e7a5942499caba8511552aae0b2714c6f33e4f
safeplace12345 <safeplace12345@gmail.com>
2021-04-11 09:19:18 +0300

    Installed Express.js

--- server.js
+++ server.js
@@ -1,34 +1,12 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
+const server = express();
 
-const server = http.createServer((req, res) => {
-  let { url, method, headers } = req;
-  if (url === "/") {
-    res.setHeader("Content-Type", "text/html");
-    res.write(
-      `<html><body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form><body></html>`
-    );
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-  return req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", message , () => {
-          res.statusCode = 302;
-          res.setHeader("Location", "/");
-          return res.end();
-      });
-    });
-  }
-  // process.exit()
-  res.setHeader("Content-Type", "text/html");
-  res.write(`<html><body>Hello from Node Js !!!!!<body></html>`);
-  res.end();
+server.use((req, res, next) => {
+  console.log("First Middleware..");
+  next();
 });
-
+server.use((req, res, next) => {
+  console.log("Second Middleware..");
+  res.send("<h1>Hello from Express Middleware</h1>");
+});
 server.listen(3000);
