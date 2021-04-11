69db086d14e65bbaf06eb47df7087839bcc9d4ea
safeplace12345 <safeplace12345@gmail.com>
2021-04-11 14:58:18 +0300

    Serving static files in public folder and html files

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const server = express();
 const bodyParser = require("body-parser");
+const path = require('path')
 
+const rootDir = require('./utils/path')
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 // Body parser
 server.use(bodyParser.urlencoded({ extended: false }));
-
-server.use((req, res, next) => {
-  console.log("First Middleware..");
-  next(); // Always use it if you need to travel through middleware
-});
+// Static files( css,js,images) middleware
+server.use(express.static(path.join(rootDir, 'public')));
+ // Routing
 server.use("/admin", adminRoutes);
 server.use(shopRoutes);
 server.use((req, res) => {
-  res.status(404).send(`<h4>Page Not found</h4>`);
+  res.status(404).sendFile(path.join(rootDir , 'views' , '404.html'));
 });
+
+// Listener
 server.listen(3000);
