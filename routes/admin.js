30db0fe83f96d0f12c3587f731334493e34f6a0a
safeplace12345 <safeplace12345@gmail.com>
2021-04-12 14:36:09 +0300

    Using Pug.js as template

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const router = express.Router();
 const path = require('path')
+
+const products = []
 // All redirect to /admin/...
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, '..' , 'views' , 'add-product.html')) 
+  res.render('add-product') 
   // this is one way of using the path
 });
 router.post("/products", (req, res, next) => {
+  products.push({product : req.body})
   res.redirect('/')
 });
 
-module.exports = router;
+module.exports = {
+  router,
+  products
+};
--- routes/shop.js
+++ routes/shop.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const path = require('path')
 const rootDir = require("../utils/path");
+const adminRoutes = require("./admin");
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, '..' , 'views' , 'shop.html'));
+  const products = adminRoutes.products
+  console.log(products)
+  res.render('shop',{products})
 });
 module.exports = router;
