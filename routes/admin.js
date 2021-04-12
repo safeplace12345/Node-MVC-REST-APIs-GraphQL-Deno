47f60b7504e495dd22bd9351d067e0d0055faafa
safeplace12345 <safeplace12345@gmail.com>
2021-04-12 16:21:29 +0300

    Fully implimented Pug templating engine

--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,4 @@
+const { json } = require('express');
 const express = require('express')
 const router = express.Router();
 const path = require('path')
@@ -5,11 +6,11 @@ const path = require('path')
 const products = []
 // All redirect to /admin/...
 router.get("/add-product", (req, res, next) => {
-  res.render('add-product') 
+  res.render('add-product', {title : 'Add-Product', path: '/admin/add-product'}) 
   // this is one way of using the path
 });
 router.post("/products", (req, res, next) => {
-  products.push({product : req.body})
+  products.push(JSON.parse(JSON.stringify(req.body)))
   res.redirect('/')
 });
 
