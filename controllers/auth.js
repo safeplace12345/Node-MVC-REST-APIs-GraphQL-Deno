9e287c59ec786f7c0a6c4c549a9c9d2f2ed292d7
GameOver <websupport@cubechallenges.com>
2021-08-25 11:17:22 +0300

    Added working cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const getLogin = (req, res, next) => {
     //Retrieve cookies
-    req.isLoggedin = (req.get('Cookie').split('=')[1])
+    req.isLoggedin = (req.get('Cookie').split('=')[1]) === 'true'
+    console.log(req.isLoggedin)
     res.render('auth/login',{
         path : '/login',
         pageTitle : "Login",
@@ -10,6 +11,11 @@ const getLogin = (req, res, next) => {
 };
 const postLogin = (req, res, next) => {
     res.setHeader('Set-Cookie',"isLoggedin=true")
+    // cookie options :
+    // Secure --> For https protocol only
+    // HttpOnly --> For http protocol only and cookies not read from clientside js
+    // Max-Age = number--> For duration , always mesured in seconds
+    // Expires = date --> For duration , always mesured in a standard http date pattern
     res.redirect("/login")
 };
 
