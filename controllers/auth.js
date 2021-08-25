4da3b7c10a6910cc8f0fcfd9629c9fc4629e732e
GameOver <websupport@cubechallenges.com>
2021-08-25 12:09:27 +0300

    Memory sessions succesfully applied

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,24 +1,29 @@
 const getLogin = (req, res, next) => {
     //Retrieve cookies
-    req.isLoggedin = (req.get('Cookie').split('=')[1]) === 'true'
-    console.log(req.isLoggedin)
-    res.render('auth/login',{
-        path : '/login',
-        pageTitle : "Login",
-        userName : "Undefined",
-        isAuthenticated : req.isLoggedin
-    })
+    // req.isLoggedin = (req.get('Cookie').split('=')[1]) === 'true'
+    // Retrieve a session
+    // req.session.isLoggedin
+    res.render("auth/login", {
+        path: "/login",
+        pageTitle: "Login",
+        userName: "Undefined",
+        isAuthenticated: req.session.isLoggedin,
+    });
 };
 const postLogin = (req, res, next) => {
-    res.setHeader('Set-Cookie',"isLoggedin=true")
+    // create a session on login
+    req.session.isLoggedin = true;
+
     // cookie options :
+    // res.setHeader('Set-Cookie',"isLoggedin=true")
     // Secure --> For https protocol only
     // HttpOnly --> For http protocol only and cookies not read from clientside js
     // Max-Age = number--> For duration , always mesured in seconds
     // Expires = date --> For duration , always mesured in a standard http date pattern
-    res.redirect("/login")
+    res.redirect("/login");
 };
 
 module.exports = {
-    getLogin,postLogin
-}
+    getLogin,
+    postLogin,
+};
--- server.js
+++ server.js
@@ -1,69 +1,80 @@
 const express = require("express");
-const server = express();
-const bodyParser = require("body-parser");
 const path = require("path");
+const bodyParser = require("body-parser");
 const { LocalStorage } = require("node-localstorage");
 const mongoose = require("mongoose");
-const localStorage = new LocalStorage("./scratch");
+const session = require("express-session");
 
 const rootDir = require("./utils/path");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const errorControllers = require("./controllers/error");
 const authRoutes = require("./routes/auth");
 
+const errorControllers = require("./controllers/error");
 const User = require("./models/users");
 
+const localStorage = new LocalStorage("./scratch");
+const server = express();
+
+// Custom middleware
 const userMidWare = async (req, res, next) => {
     // get username if it exists on the query already
-    let userCreds = "name=chris"
+    let userCreds = "name=chris";
     if (req.query.name) {
         let userCreds = req.query.name;
-    }    //Trim query for userName
-        const strLength = userCreds.length;
-        userCreds =
-            userCreds.substr(0, 1).toUpperCase() +
-            userCreds.substr(1, strLength - 1);
-        // Create user
+    } //Trim query for userName
+    const strLength = userCreds.length;
+    userCreds =
+        userCreds.substr(0, 1).toUpperCase() +
+        userCreds.substr(1, strLength - 1);
+    // Create user
 
-
-        return await User.findById("611cf85da8861d6014ea6142")
-            .then((user) => {
-                // Validate user
-                if (!user) {
-                    let user = new User({
-                        name: userCreds,
-                        email: userCreds + "@gmail.com",
-                        address: "U.S.A",
-                        cart: [],
-                    });
-                    return user.save();
-                }
-            })
-            .then((result) => {
-               return User.findById("611cf85da8861d6014ea6142").then((user) => {
-                    req.user = user;
-                    if (localStorage) {
-                        localStorage.setItem("userName", user.name);
-                        return next();
-                    }
-                    return next();
+    return await User.findById("611cf85da8861d6014ea6142")
+        .then((user) => {
+            // Validate user
+            if (!user) {
+                let user = new User({
+                    name: userCreds,
+                    email: userCreds + "@gmail.com",
+                    address: "U.S.A",
+                    cart: [],
                 });
+                return user.save();
+            }
+        })
+        .then((result) => {
+            return User.findById("611cf85da8861d6014ea6142").then((user) => {
+                req.user = user;
+                if (localStorage) {
+                    localStorage.setItem("userName", user.name);
+                    return next();
+                }
+                return next();
             });
-
+        });
 };
+
 server.set("view engine", "ejs");
 server.set("views", "views");
+
+
+//              Middleware
 // Body parser
 server.use(bodyParser.urlencoded({ extended: false }));
-
 // Static files( css,js,images) middleware
 server.use(express.static(path.join(rootDir, "public")));
+// sessions
+server.use(
+    session({ secret: "qwerty", resave: false, saveUninitialized: false })
+);
+
 // Routing
 server.use("/admin", userMidWare, adminRoutes.router);
 server.use("/clients", userMidWare, shopRoutes);
 server.use(authRoutes);
 server.use(errorControllers.get404Page);
+
+
 // Listener
 mongoose
     .connect(
